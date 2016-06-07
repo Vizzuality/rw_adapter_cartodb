df71e054485c5e90627655cc7a337061d7ddf10f
Sebastian Schkudlara <sebastian.schkudlara@gmail.com>
2016-06-07 13:13:35 +0200

    Updates for default limit

--- app/models/query_params.rb
+++ app/models/query_params.rb
@@ -7,11 +7,7 @@ class QueryParams < Hash
       not_filter: filter_params(params['filter_not']) || nil,
       aggr_by: params['aggr_by'].present? ? params['aggr_by'] : [],
       aggr_func: params['aggr_func'] || nil,
-      limit: params['limit'] ||= if (params['filter'].present? || params['not_filter'].present? || params['aggr_func'].present?)
-                                   ['all']
-                                 else
-                                   [1]
-                                 end
+      limit: params['limit'] ||= standard_limit(params)
     }
 
     super(sanitized_params)
@@ -33,5 +29,13 @@ class QueryParams < Hash
 
     def validate_params(filter)
       filter.include?('==') || filter.include?('>=') || filter.include?('>>') || filter.include?('<=') || filter.include?('<<') || filter.include?('><')
+    end
+
+    def standard_limit(params)
+      if params['select'].present? || params['filter'].present? || params['not_filter'].present? || params['aggr_func'].present?
+        ['all']
+      else
+        [1]
+      end
     end
 end
