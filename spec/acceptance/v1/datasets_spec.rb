f888a871c1dca904c94aca62081dc67171189a0a
Sebastian Schkudlara <sebastian.schkudlara@gmail.com>
2016-08-10 10:00:12 +0200

    Use JsonApi adapter

--- spec/acceptance/v1/datasets_spec.rb
+++ spec/acceptance/v1/datasets_spec.rb
@@ -31,23 +31,23 @@ module V1
         it 'Allows access cartoDB data with default limit 1' do
           post "/query/#{dataset_id}", params: params
 
-          data = json['data'][0]
+          data = json['attributes']['data'][0]
 
           expect(status).to eq(200)
-          expect(data['cartodb_id']).not_to be_nil
+          expect(data['cartodb-id']).not_to be_nil
           expect(data['pcpuid']).not_to     be_nil
-          expect(data['the_geom']).to       be_present
-          expect(json['data'].length).to    eq(1)
+          expect(data['the-geom']).to       be_present
+          expect(json['attributes']['data'].length).to    eq(1)
         end
 
         it 'Allows access cartoDB data with default limit 1 for tables url' do
           post "/query/#{dataset_id}", params: tables_params
 
-          data = json['data'][0]
+          data = json['attributes']['data'][0]
 
           expect(status).to eq(200)
-          expect(data['cartodb_id']).to  be(1)
-          expect(json['data'].length).to eq(1)
+          expect(data['cartodb-id']).to  be(1)
+          expect(json['attributes']['data'].length).to eq(1)
         end
       end
 
@@ -55,90 +55,90 @@ module V1
         it 'Allows access all available cartoDB data with limit all' do
           post "/query/#{dataset_id}?limit=all", params: params
 
-          data = json['data'][0]
+          data = json['attributes']['data'][0]
 
           expect(status).to eq(200)
-          expect(data['cartodb_id']).not_to be_nil
+          expect(data['cartodb-id']).not_to be_nil
           expect(data['pcpuid']).not_to     be_nil
-          expect(data['the_geom']).to       be_present
-          expect(json['data'].length).to    eq(5)
+          expect(data['the-geom']).to       be_present
+          expect(json['attributes']['data'].length).to    eq(5)
         end
 
         it 'Allows access cartoDB data with order ASC' do
           post "/query/#{dataset_id}?sql=select * from public.carts_test_endoint order by cartodb_id ASC", params: params
 
-          data = json['data'][0]
+          data = json['attributes']['data'][0]
 
           expect(status).to eq(200)
-          expect(data['cartodb_id']).to eq(1)
+          expect(data['cartodb-id']).to eq(1)
         end
 
         it 'Allows access cartoDB data with order DESC' do
           post "/query/#{dataset_id}?sql=select * from public.carts_test_endoint order by cartodb_id DESC", params: params
 
-          data = json['data'][0]
+          data = json['attributes']['data'][0]
 
           expect(status).to eq(200)
-          expect(data['cartodb_id']).to eq(5)
+          expect(data['cartodb-id']).to eq(5)
         end
 
         it 'Allows access cartoDB data details with select and order using fs' do
           post "/query/#{dataset_id}?outFields=cartodb_id,pcpuid&orderByFields=pcpuid ASC", params: params
 
-          data = json['data'][0]
+          data = json['attributes']['data'][0]
 
           expect(status).to eq(200)
-          expect(data['cartodb_id']).to   eq(5)
+          expect(data['cartodb-id']).to   eq(5)
           expect(data['pcpuid']).not_to   be_nil
-          expect(data['the_geom']).not_to be_present
+          expect(data['the-geom']).not_to be_present
         end
 
         it 'Allows access cartoDB data details with select, filter and order DESC' do
           post "/query/#{dataset_id}?sql=select cartodb_id,pcpuid from public.carts_test_endoint where cartodb_id in (1,2,4,5) and pcpuid between '350558' and '9506590' order by pcpuid DESC", params: params
 
-          data = json['data'][0]
+          data = json['attributes']['data'][0]
 
           expect(status).to eq(200)
-          expect(data['cartodb_id']).to   eq(1)
+          expect(data['cartodb-id']).to   eq(1)
           expect(data['pcpuid']).to       eq('500001')
-          expect(data['the_geom']).not_to be_present
+          expect(data['the-geom']).not_to be_present
         end
 
         it 'Allows access cartoDB data details with select, filter_not and order' do
           post "/query/#{dataset_id}?sql=select cartodb_id,pcpuid from public.carts_test_endoint where cartodb_id != 4 and pcpuid not between '500001' and '9506590' order by pcpuid ASC", params: params
 
-          data = json['data'][0]
+          data = json['attributes']['data'][0]
 
           expect(status).to eq(200)
-          expect(data['cartodb_id']).to   eq(5)
+          expect(data['cartodb-id']).to   eq(5)
           expect(data['pcpuid']).not_to   be_nil
-          expect(data['the_geom']).not_to be_present
+          expect(data['the-geom']).not_to be_present
         end
 
         it 'Allows access cartoDB data details for all filters using fs' do
           post "/query/#{dataset_id}?outFields=cartodb_id,pcpuid&where=cartodb_id < 5 and pcpuid >= '350558' and cartodb_id != 4 and pcpuid not between '350640' and '9506590'&orderByFields=pcpuid DESC", params: params
 
-          data = json['data']
+          data = json['attributes']['data']
 
           expect(status).to eq(200)
           expect(data.size).to             eq(1)
-          expect(data[0]['cartodb_id']).to eq(2)
+          expect(data[0]['cartodb-id']).to eq(2)
           expect(data[0]['pcpuid']).not_to be_nil
-          expect(data[0]['the_geom']).to   be_nil
+          expect(data[0]['the-geom']).to   be_nil
         end
 
         it 'Allows access cartoDB data with limit rows' do
           post "/query/#{dataset_id}?limit=2", params: params
 
           expect(status).to eq(200)
-          expect(json['data'].length).to eq(2)
+          expect(json['attributes']['data'].length).to eq(2)
         end
 
         it 'Allows access cartoDB data with limit rows as array filter' do
           post "/query/#{dataset_id}?limit[]=3", params: params
 
           expect(status).to eq(200)
-          expect(json['data'].length).to eq(3)
+          expect(json['attributes']['data'].length).to eq(3)
         end
       end
 
@@ -147,8 +147,8 @@ module V1
           post "/fields/#{dataset_id}", params: params
 
           expect(status).to eq(200)
-          expect(json['fields']).to         be_present
-          expect(json['table_name']).not_to eq('data')
+          expect(json['attributes']['fields']).to         be_present
+          expect(json['attributes']['table-name']).not_to eq('data')
         end
       end
     end
